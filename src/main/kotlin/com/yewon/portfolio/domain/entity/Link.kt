a4662016d150ece6648a81daf45250a622798709
Kimyewon <kyw010103@gmail.com>
2024-10-06 19:01:15 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/yewon/portfolio/domain/entity/Link.kt
+++ src/main/kotlin/com/yewon/portfolio/domain/entity/Link.kt
@@ -1,12 +1,19 @@
 package com.yewon.portfolio.domain.entity
 
 import jakarta.persistence.*
+import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain.Strategy.Content
 
 @Entity
-class Link : BaseEntity() {
+class Link(name: String, content: String, isActive: Boolean) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "link_id")
     var id: Long? = null
+
+    var name: String = name
+
+    var content: String =content
+
+    var isActive: Boolean = isActive
 }
