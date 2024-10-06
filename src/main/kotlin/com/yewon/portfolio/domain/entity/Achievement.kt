2a2dfda419c8f4631675bcc71b7c8f36b37808cf
Kimyewon <kyw010103@gmail.com>
2024-10-06 18:15:28 +0900

    엔치치 개발 - 연관관계 없음

--- src/main/kotlin/com/yewon/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/yewon/portfolio/domain/entity/Achievement.kt
@@ -3,10 +3,27 @@ package com.yewon.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Achievement : BaseEntity() {
+class Achievement(
+        title: String,
+        description: String,
+        achievedDate: LocalDate?,
+        host: String,
+        isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "achievement_id")
     var id: Long? = null
+
+    var title: String = title
+
+    var description: String = description
+
+    var achievedDate: LocatDate? = achievedDate
+
+    var host: String = host
+
+    var isActive: Boolean = isActive
+
 }
