ac8c02f291ed841a39e6b13482633ec8ca1dc6ed
Kimyewon <kyw010103@gmail.com>
2024-10-07 20:10:46 +0900

    리포지토리 개발

--- src/main/kotlin/com/yewon/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/yewon/portfolio/domain/repository/ProjectRepository.kt
@@ -2,5 +2,11 @@ package com.yewon.portfolio.domain.repository
 
 import com.yewon.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface ProjectRepository : JpaRepository<Project, Long>
+interface ProjectRepository : JpaRepository<Project, Long> {
+
+    fun findAllByIsActive(isActive: Boolean): List<Project>
+
+    override fun findById(id: Long): Optional<Project>
+}
